7d1fa850a59c0c1cb7da8d2b02ad29dc10b4bb0b
Roberto Núñez <robertonc1599@gmail.com>
2024-04-14 22:08:09 -0500

    Configurations for Dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Metadata } from "next";
+import { Sidebar } from "../components";
+
+export const metadata: Metadata = {
+  title: "Dashboard - Administrativo",
+  description: "This is the Counter Dashboard",
+  keywords: ["Dashboard", "Counter"],
+};
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+      <div className="flex">
+        <Sidebar />
+        <div className="p-2 w-full text-slate-900">{children}</div>
+      </div>
+    </div>
+  );
+}
