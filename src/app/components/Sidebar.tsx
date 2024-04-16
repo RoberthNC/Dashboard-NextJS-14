4c2a3994fd9bb0a68470c075c379811d8997e4a6
Roberto Núñez <robertonc1599@gmail.com>
2024-04-15 22:32:33 -0500

    Getting the pokemons from API and PokemonCard has been created

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5";
+import {
+  IoBrowsersOutline,
+  IoCalculator,
+  IoFootball,
+  IoLogoReact,
+} from "react-icons/io5";
 import { SidebarMenuItem } from "./";
 
 const menuItems = [
@@ -15,6 +20,12 @@ const menuItems = [
     title: "Counter",
     subtitle: "Contador Client Side",
   },
+  {
+    path: "/dashboard/pokemons",
+    icon: <IoFootball size={40} />,
+    title: "Pokémons",
+    subtitle: "Generación Estática",
+  },
 ];
 
 export function Sidebar() {
@@ -22,7 +33,7 @@ export function Sidebar() {
     <div
       id="menu"
       style={{ width: "400px" }}
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll"
     >
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-center gap-2 text-lg md:text-2xl font-bold text-white">
