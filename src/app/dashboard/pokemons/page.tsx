4827c1ef50a2b4fa85756c5be62a26ff15ffef31
Roberto Núñez <robertonc1599@gmail.com>
2024-04-17 18:30:05 -0500

    Title of pokemons page has been modified

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -26,7 +26,8 @@ export default async function PokemonsPage() {
   return (
     <div className="flex flex-col">
       <span className="text-5xl my-2">
-        Listado de Pokémons <small>estático</small>
+        Listado de Pokémons{" "}
+        <small className="text-blue-500 font-bold">Estáticos</small>
       </span>
       <PokemonGrid pokemons={pokemons} />
     </div>
