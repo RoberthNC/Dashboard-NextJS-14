d74f4f7095c293b79734b6e4d97a22919a65ae76
Roberto Núñez <robertonc1599@gmail.com>
2024-04-18 22:12:29 -0500

    Validation for no favorites

--- src/pokemons/pokemons/FavoritePokemons.tsx
+++ src/pokemons/pokemons/FavoritePokemons.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import { useState } from "react";
 import { useAppSelector } from "@/store";
 import { PokemonGrid } from "./PokemonGrid";
+import { IoHeartOutline } from "react-icons/io5";
+import { SimplePokemon } from "../interfaces/simple-pokemon";
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector((state) =>
     Object.values(state.pokemons)
   );
-  return <PokemonGrid pokemons={favoritePokemons} />;
+  const [pokemons, setPokemons] = useState<SimplePokemon[]>(favoritePokemons);
+  return (
+    <>
+      {pokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <PokemonGrid pokemons={favoritePokemons} />
+      )}
+    </>
+  );
+};
+
+export const NoFavorites = () => {
+  return (
+    <div className="flex flex-col h-[50vh] items-center justify-center">
+      <IoHeartOutline size={100} className="text-red-500" />
+      <span>No hay pokémons favoritos</span>
+    </div>
+  );
 };
